dbc0902cdfc6d6d6cc26b5db4c430f1db3063af4
niclan051 <89274627917m@gmail.com>
2024-02-22 15:37:46 +0300

    enemy movement

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+    private NavMeshAgent _navMeshAgent;
+
+    private void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        _navMeshAgent.destination = player.transform.position;
+    }
+}
